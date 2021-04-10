b3e8bd3c4277f23fc2f4e6d3bbafe26d4675aa5d
Aaron Ferris Richardson <aaron.richardson@gmail.com>
2021-04-10 13:38:16 +0100

    add (Game): Constant Specific Method for Nouns

--- src/main/java/RegexGame/Game.java
+++ src/main/java/RegexGame/Game.java
@@ -1,6 +1,7 @@
 package RegexGame;
 
 import java.util.Scanner;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,8 +22,8 @@ public class Game {
         String noun = parseForNoun(input);
 
         // INITIAL: more flexible implementation
-        if (noun == "room") {
-            if (verb == "look") {
+        if (noun.equals("room")) {
+            if (verb.equals("look")) {
                 System.out.println(room.look());
             }
         }
@@ -52,19 +53,37 @@ public class Game {
     private static String parseForNoun(String input) {
         String output = null;
 
-        Pattern roomPattern = Pattern.compile("o*m+");
-        Matcher roomMatcher = roomPattern.matcher(input);
-
-        Pattern personPattern = Pattern.compile("(m+e+|s*e*l*f+|p*l*a*y*e*r+)");
-        Matcher playerMatcher = personPattern.matcher(input);
-
-        if(roomMatcher.find()) {
+        if(DetectTopic.ROOM.compute(input)) {
             output = "room";
         }
-        if(playerMatcher.find()) {
+        if(DetectTopic.PLAYER.compute(input)) {
             output = "player";
         }
         return output;
+    }
+
+    public enum DetectTopic { // Constant Specific Method
+        /* I see now this implementation has limitations.
+            e.g will have to ask for "Object 1", "Person 1"
+         */
+        ROOM (b -> findByPattern("o*m+", b)),
+        PLAYER (b -> findByPattern("(m+e+|s*e*l*f+|p*l*a*y*e*r+)", b));
+
+        private final Function<String,Boolean> validPatterns ;
+        DetectTopic(Function<String,Boolean> validPatterns) {
+            this.validPatterns = validPatterns;
+        }
+
+        private static Boolean findByPattern(String regexPattern, String input) {
+            Pattern pattern = Pattern.compile(regexPattern);
+            Matcher matcher = pattern.matcher(input);
+
+            return matcher.find();
+        }
+
+        public Boolean compute(String x) {
+            return validPatterns.apply(x);
+        }
     }
 
 }
